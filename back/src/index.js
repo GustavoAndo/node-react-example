75e30accd1a175274759a37ddb233b76cdeb1f7f
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-08-29 14:43:35 -0300

    create user back-end

--- back/src/index.js
+++ back/src/index.js
@@ -19,21 +19,37 @@ app.use(express.json())
 
 // testes
 
+/*
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.post('/teste', async(req, res) => {
     try {
-        const novaTarefa = await bd.query(
+        const novoUsuario = await bd.query(
             "INSERT INTO usuarios (nome, email, idade, dinheiro, data_cadastrado) VALUES ('Gustavo', 'gustavo.k.ando@gmail.com', 19, 0.50, 'now')")
     } catch (error) {
         console.error(error)
     }
 })
+*/
 
 // create
 
+app.post('/adicionarUsuario', async(req, res) => {
+    try {
+        const { nome } = req.body
+        const { email } = req.body
+        const { idade } = req.body
+        const { dinheiro } = req.body
+
+        const novoUsuario = await bd.query(
+            "INSERT INTO usuarios (nome, email, idade, dinheiro, data_cadastrado) VALUES ($1, $2, $3, $4, 'now') RETURNING *", [nome, email, idade, dinheiro]
+        )
+    } catch (error) {
+        console.error(error)
+    }
+})
 
 // read
 
